e273450339775766a53f2b981acc686c8aa4a1c8
Olasunkanmi Oyinlola <olasunkanmi@regovtech.com>
2025-04-09 08:03:15 +0800

    feat(ui): Enhance project display and filter by type
    - Update project page to display open-source projects separately.
    - Filter projects in showcase and project pages to exclude Open type.
    - Update author bio with Reactjs
    - Update project descriptions for clarity and accuracy.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,28 +9,62 @@ export const metadata = {
 
 export default async function Projects() {
   return (
-    <div className="container divide-y divide-gray-200 dark:divide-gray-700">
-      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-          Projects
-        </h1>
-      </div>
-      <div className=" pt-12">
-        <div className="-m-4 flex flex-wrap">
-          {projectsData.map((d) => (
-            <ProjectCard
-              key={d.title}
-              title={d.title}
-              description={d.description}
-              titleLink={d.titleLink}
-              skills={d.skills}
-              designPattern={d.designPattern}
-              links={d.links}
-              icons={d.icons}
-            />
-          ))}
+    <>
+      {" "}
+      <div className="container divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Open Source Projects
+          </h1>
+        </div>
+        <div className=" pt-12">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData
+              .filter((d) => d.type === "Open")
+              .map((d) => (
+                <ProjectCard
+                  key={d.title}
+                  title={d.title}
+                  description={d.description}
+                  titleLink={d.titleLink}
+                  skills={d.skills}
+                  designPattern={d.designPattern}
+                  links={d.links}
+                  icons={d.icons}
+                  type={d.type}
+                />
+              ))}
+          </div>
         </div>
       </div>
-    </div>
+      <div className="container divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Projects
+          </h1>
+        </div>
+        <div className=" pt-12">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData
+              .slice(0, 4)
+              .filter((d) => d.type !== "Open")
+              .map((d) => (
+                <ProjectCard
+                  key={d.title}
+                  title={d.title}
+                  description={d.description}
+                  titleLink={d.titleLink}
+                  skills={d.skills}
+                  links={d.links}
+                  icons={d.icons}
+                  designPattern={d.designPattern}
+                  type={d.type}
+                  className="md relative grow p-4 md:w-1/2 md:grow-0"
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
