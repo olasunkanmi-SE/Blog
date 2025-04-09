e8cc56362c8a4e982246a9e299d5ddd21f25583b
Olasunkanmi Oyinlola <olasunkanmi@regovtech.com>
2025-04-09 08:17:16 +0800

    docs(data): refine projects data
    - Update skills for Codebuddy to Reactjs.
    - Update skills for Restaurant to Reactjs.
    - Refine skills for ts-codebase-analyzer to include AST, Typescript compiler, and File Systems.
    - Add Angularjs to skills for Pickles Live Auctions.

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -18,7 +18,7 @@ const projectsData: ProjectData[] = [
    AI-powered coding assistant that employs  the use of Large Language Models (LLMs), context retrieval, code indexing, and workflow orchestration to generate code, automate tasks, and improve developers' output.`,
     titleLink: "https://github.com/olasunkanmi-SE/codebuddy",
     skills:
-      "Event-Driven architecture, Workflow Orchestration, React, Generative AI, RAG, VectorDB, SQLite, Abstract Synthax Tree (AST), MCP, CodeIndexing",
+      "Event-Driven architecture, Workflow Orchestration, Reactjs, Generative AI, RAG, VectorDB, SQLite, Abstract Synthax Tree (AST), MCP, CodeIndexing, ",
     designPattern: "Orchestration, Event-Driven, Command Pattern.",
 
     links: [
@@ -32,7 +32,7 @@ const projectsData: ProjectData[] = [
       "This project is a comprehensive restaurant management system with a NestJS backend and React frontend. The application takes advantage of Nestjs inversion of control IOC, containers, decorators, and its modular architecture approach to deliver a loosely coupled extensible web application.",
     titleLink: "https://github.com/olasunkanmi-SE/restaurant",
     skills:
-      "Typescript, React, NestJs, MongoDB, RBAC, Authentication, State management",
+      "Typescript, Reactjs, NestJs, MongoDB, RBAC, Authentication, State management",
     designPattern: "Clean Architecture, Inversion of Control.",
     links: [
       { title: "GitHub", href: "https://github.com/olasunkanmi-SE/restaurant" },
@@ -45,7 +45,8 @@ const projectsData: ProjectData[] = [
     description:
       "A powerful tool for parsing and analyzing TypeScript codebases using the TypeScript Abstract Syntax Tree (AST). This npm package is useful for codeIndexing and context retrieval while building Generative AI applications with Retrival Augumented Generator RAG, that serves as memory providers to AI agents.",
     titleLink: "https://github.com/olasunkanmi-SE/ts-codebase-analyzer",
-    skills: "Typescript",
+    skills:
+      "Typescript Abstract synthax tree AST, Typescript compiler, Files Systems ",
     designPattern: "Singleton",
     links: [
       {
@@ -61,7 +62,7 @@ const projectsData: ProjectData[] = [
       "Pickles Live Auctions provides a dynamic online car auctioning platform. It is a microservice, built on event driven architecture, with KAFKA, docker, kubernetes. It serves more than a million requests per day.",
     titleLink: "https://github.com/olasunkanmi-SE/ts-codebase-analyzer",
     skills:
-      "Event-Driven architecture, CQRS, (CI/CD), SDKs, Containerization, Message Broker, postgreSQL, MongoDB, Microservices, Web Application Security, AWS",
+      "Event-Driven architecture, CQRS, (CI/CD), SDKs, Containerization, Message Broker, postgreSQL, MongoDB, Microservices, Web Application Security, AWS, Angularjs",
     designPattern:
       "Event-Driven, Domain-Driven, Clean Architecture, Hexagonal Architecture",
     links: [
